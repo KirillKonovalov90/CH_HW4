0dac37195c45daad00bfed27b155d8996a7e34df
Kirill <wakened90@gmail.com>
2023-01-19 21:30:13 +0300

    sumOfDigits Works!

---
+++ sumOfDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿            // Программа принимает число и возвращает сумму его цифр
+
+int sumOfDigits (int number)
+{
+    int digit, summ = 0;
+
+    while (number != 0)
+    {
+        digit = number % 10;
+        summ = summ + digit;
+        number /= 10;
+    }
+    return summ;
+}
+int num;
+
+Console.Write("Введите число: ");
+num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(sumOfDigits(num));
