0159a9848f01fa874962a48e32bf11f8eb696075
Kirill <wakened90@gmail.com>
2023-01-19 21:17:08 +0300

    APowB working

---
+++ AToThePowerOfB/Program.cs
@@ -0,0 +1,35 @@
+﻿                // Программа принимает числа A и B и выводит результат равный A в степени B
+
+double AToThePowerOfB (int A, int B)
+{
+    double result = A;
+    if (B == 0)
+    {
+        return 1;        
+    }
+    else if (B < 0)                 //чуть усложнил программу и позволил брать отрицательные степени
+    {
+        for (int i = 1; i < B * (-1); i++)
+        {
+        result = result * A;
+        }
+        return (1 / result);
+    }
+    else
+    {
+         for (int i = 1; i < B; i++)
+        {
+        result = result * A;
+        }
+        return result;
+    }     
+}
+
+int numA, numB;
+
+Console.Write("Введите число A: ");
+numA = int.Parse(Console.ReadLine()!);
+Console.Write("Введите желаемую степень B: ");
+numB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(AToThePowerOfB (numA, numB));
